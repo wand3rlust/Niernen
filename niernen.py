a85a30aa120c9a10a7311bad691d4043200d1442
Abhijeet Kumar <83376115+wand3rlust@users.noreply.github.com>
2023-03-18 09:42:42 +0000

    Update niernen.py

--- niernen.py
+++ niernen.py
@@ -22,7 +22,7 @@ def encode_shellcode(shellcode, key):
         encoded_shellcode.append(ror_byte)
     return bytes(encoded_shellcode)
 
-#Decode shellcode using ROR, ROL, SUB, ADD, XOR
+#Decode shellcode using ROR, ROL, SUB, ADD, and XOR
 def decode_shellcode(encoded_shellcode, key):
     decoded_shellcode = bytearray()
     for i, byte in enumerate(encoded_shellcode):
@@ -46,19 +46,19 @@ def generate_key(length):
 
 def info():
     print(f"""{GREEN}
-    
-     _       _________ _______  _______  _        _______  _       
-    ( (    /|\__   __/(  ____ \(  ____ )( (    /|(  ___  )( (    /|
-    |  \  ( |   ) (   | (    \/| (    )||  \  ( || (   ) ||  \  ( |
-    |   \ | |   | |   | (__    | (____)||   \ | || (___) ||   \ | |
-    | (\ \) |   | |   |  __)   |     __)| (\ \) ||  ___  || (\ \) |
-    | | \   |   | |   | (      | (\ (   | | \   || (   ) || | \   |
-    | )  \  |___) (___| (____/\| ) \ \__| )  \  || )   ( || )  \  |
-    |/    )_)\_______/(_______/|/   \__/|/    )_)|/     \||/    )_)
-    
+                
+                           _         _________   _______    _______    _          _______    _       
+                          ( (    /|  \__   __/  (  ____ \  (  ____ )  ( (    /|  (  ____ \  ( (    /|
+                          |  \  ( |     ) (     | (    \/  | (    )|  |  \  ( |  | (    \/  |  \  ( |
+                          |   \ | |     | |     | (__      | (____)|  |   \ | |  | (__      |   \ | |
+                          | (\ \) |     | |     |  __)     |     __)  | (\ \) |  |  __)     | (\ \) |
+                          | | \   |     | |     | (        | (\ (     | | \   |  | (        | | \   |
+                          | )  \  |  ___) (___  | (____/\  | ) \ \__  | )  \  |  | (____/\  | )  \  |
+                          |/    )_)  \_______/  (_______/  |/   \__/  |/    )_)  (_______/  |/    )_)
+
     {RESET} {RED}                        
-                         Author: Abhijeet Kumar
-                      Github: github.com/wand3rlust
+                                                     Author: Abhijeet Kumar
+                                                  Github: github.com/wand3rlust
         
     {RESET}""")
 
@@ -66,73 +66,73 @@ def info():
 def main():
     info()
     while True:
-        print(f"{GREEN}1. Encode shellcode")
-        print("2. Decode shellcode")
-        print("3. Exit")
-        choice = input(f"Enter your choice [1-3]: {RESET}\n")
+        print(f"\n"
+              f"{GREEN}1. Encode shellcode\n")
+        print("2. Decode shellcode\n")
+        print("3. Exit\n")
+        choice = input(f"Enter your choice [1-3]: {RESET}")
 
         if choice == "1":
-            plaintext_shellcode = input("Enter plaintext shellcode:\n")
+            plaintext_shellcode = input("\nEnter plaintext shellcode: ")
             #Encode the user unput into UTF-8 and change from string to byte
             shellcode = plaintext_shellcode.encode()
             #Generate same length key as shellcode hex
             key = generate_key(len(shellcode))
             #Call encode_shellcode function with 2 arguments i.e, UTF-8 shellcode and key
             encoded_shellcode = encode_shellcode(shellcode, key)
-            print(f"Original shellcode (in hex): {RED}", shellcode.hex())
-            print(f"{RESET}Key (in hex): {RED}", key.hex())
-            print(f"{RESET}Encoded shellcode (in hex): {RED}", encoded_shellcode.hex())
+            print(f"\nOriginal shellcode (in hex): {RED}", shellcode.hex())
+            print(f"\n{RESET}Key (in hex): {RED}", key.hex())
+            print(f"\n{RESET}Encoded shellcode (in hex): {RED}", encoded_shellcode.hex())
             #Convert byte format to string
             encoded_shellcode = encoded_shellcode.hex()
             #Append \x after every 2nd character
             encoded_shellcode = "\\x" + "\\x".join(encoded_shellcode[i:i + 2] for i in range(0, len(encoded_shellcode), 2))
-            print(f"{RESET}Encoded shellcode (with \\x): {RED}", encoded_shellcode)
+            print(f"\n{RESET}Encoded shellcode (with \\x): {RED}", encoded_shellcode)
             print(f"{RESET}\n")
 
         elif choice == "2":
-            print(f"{GREEN}Choose the hex format:")
-            print("a. Normal hex")
-            print("b. \\x format")
-            choice = input(f"Enter your choice [ a-b]: {RESET}\n")
+            print(f"\n{GREEN}Choose the hex format: ")
+            print("\na. Normal hex")
+            print("\nb. \\x format")
+            choice = input(f"\nEnter your choice [a-b]: {RESET}")
 
             if choice == "a":
                 #Convert user input hex to byte
-                encoded_shellcode = bytes.fromhex(input("Enter encoded shellcode (in hex):\n"))
+                encoded_shellcode = bytes.fromhex(input("\nEnter encoded shellcode (in hex): "))
                 #Convert user input key into byte
-                key = bytes.fromhex(input("Enter key (in hex):\n"))
+                key = bytes.fromhex(input("\nEnter key (in hex): "))
                 #Call decode_shellcode function with 2 arguments i.e, shellcode and key
                 decoded_shellcode = decode_shellcode(encoded_shellcode, key)
                 #Convert encoded shellcode to hex and display
-                print(f"Encoded shellcode (in hex): {RED}", encoded_shellcode.hex())
+                print(f"\nEncoded shellcode (in hex): {RED}", encoded_shellcode.hex())
                 #Convert key to hex and display
-                print(f"{RESET}Key (in hex): {RED}", key.hex())
+                print(f"\n{RESET}Key (in hex): {RED}", key.hex())
                 #Convert decoded shellcode to hex and display
-                print(f"{RESET}Decoded shellcode (in hex): {RED}", decoded_shellcode.hex())
+                print(f"\n{RESET}Decoded shellcode (in hex): {RED}", decoded_shellcode.hex())
                 print("\n")
 
             elif choice == "b":
-                encoded_shellcode = input("Enter encoded shellcode (with \\x):\n")
+                encoded_shellcode = input("\nEnter encoded shellcode (with \\x): ")
                 #Remove \x from hex shellcode and convert to byte
                 encoded_shellcode = bytes.fromhex(encoded_shellcode.replace("\\x", ""))
                 #Convert encoded shellcode to hex and display
-                print(f"Encoded shellcode (without \\x): {RED}", encoded_shellcode.hex())
+                print(f"\nEncoded shellcode (without \\x): {RED}", encoded_shellcode.hex())
                 #Convert user input key into byte
-                key = bytes.fromhex(input(f"{RESET}Enter key (in hex):\n"))
+                key = bytes.fromhex(input(f"\n{RESET}Enter key (in hex): "))
                 #Call decode_shellcode function with 2 arguments i.e, shellcode and key
                 decoded_shellcode = decode_shellcode(encoded_shellcode, key)
-                print(f"Decoded shellcode (in hex): {RED}", decoded_shellcode.hex())
+                print(f"\nDecoded shellcode (in hex): {RED}", decoded_shellcode.hex())
                 print(f"{RESET}\n")
 
             else:
                 print(f"{RED}Invalid selection, start again{RESET}\n")
 
         elif choice == "3":
-            print(f"{GREEN}Sayonara...{RESET}")
+            print(f"\n{GREEN}Sayonara...{RESET}\n")
             break
 
         else:
-            print(f"{RED}404: choice not found{RESET}")
-            print("\n")
+            print(f"\n{RED}404: choice not found{RESET}\n")
 
 if __name__ == "__main__":
     main()
